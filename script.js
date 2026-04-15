ac1495185c7d200d4e02c8b7ae4077dfd43f3d00
Sarah03 <271220460+SarahO3@users.noreply.github.com>
2026-04-15 09:00:11 +0100

    Made changes to script.js
    Practiced more javascript to level up

--- script.js
+++ script.js
@@ -1,3 +1,133 @@
+
+// checking for prime number from number 2 to 10
+// let num = 10
+
+// outer:
+// for(let i = 2; i <= num; i++){
+//   for(let j = 2; j < i; j++){
+//     if(i % j === 0){
+//       continue outer
+//     }
+//   }
+//   alert(i)
+// }
+
+
+// array
+
+// let styles = ["jazz", "blues"]
+// styles.push("roch-n-roll")
+
+// styles[Math.floor(styles.length - 1) / 2] = "classics"
+
+
+// alert(styles.shift())
+
+
+// styles.unshift("rap", "raggae")
+
+// console.log(styles)
+
+
+
+
+
+// count down 10
+/*
+Add code to loop from 10 down to 0. We've provided you with an initializer — let i = 10;.
+For each iteration, create a new paragraph and append it to the output <div>, which we've selected using const output = document.querySelector('.output');. We've provided you with three code lines inside comments that need to be used somewhere inside the loop:
+const para = document.createElement('p'); — creates a new paragraph.
+output.appendChild(para); — appends the paragraph to the output <div>.
+para.textContent = — makes the text inside the paragraph equal to whatever you put on the right-hand side, after the equals sign.
+For the different iteration numbers listed below, write code to insert the required text inside the paragraph (you'll need a conditional statement and multiple para.textContent = lines):
+If the number is 10, print "Countdown 10" to the paragraph.
+If the number is 0, print "Blast off!" to the paragraph.
+For any other number, print just the number to the paragraph.
+*/
+
+const output = document.querySelector(".output");
+output.textContent = "";
+
+let i = 10;
+
+while (i >= 0) {
+  const para = document.createElement('p');
+
+  if (i === 10) {
+    para.textContent = `Countdown ${i}`;
+  } else if (i === 0) {
+    para.textContent = "Blast off!";
+  } else {
+    para.textContent = i;
+  }
+
+  output.appendChild(para);
+  i--;
+}
+
+
+
+// guest list to let in
+
+
+const people = [
+  "Chris", "Anne", "Colin", "Terri",
+  "Phil", "Lola", "Sam", "Kay", "Bruce",
+];
+
+const admitted = document.querySelector(".admitted");
+const refused = document.querySelector(".refused");
+
+let admittedList = [];
+let refusedList = [];
+
+for (const person of people) {
+  if (person === "Phil" || person === "Lola") {
+    refusedList.push(person);
+  } else {
+    admittedList.push(person);
+  }
+}
+
+// take each item in admitted and refused array and join them with comma, then add fullstop to the last item
+admitted.textContent = "Admit: " + admittedList.join(", ") + ".";
+refused.textContent = "Refuse: " + refusedList.join(", ") + ".";
+
+/*
+Sum input numbers
+importance: 4
+Write the function sumInput() that:
+
+Asks the user for values using prompt and stores the values in the array.
+Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+Calculates and returns the sum of array items.
+P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+
+Run the demo
+*/
+
+function sumInput(){
+  let arrs = []
+
+  while (true) {
+let userInput = prompt("Please enter a number", 0)
+
+
+// should we cancel
+if(userInput === "" || userInput === null || !isFinite(userInput)) break
+
+  arrs.push(+userInput)
+  }
+
+let sum = 0
+for (let arr of arrs){
+  sum += arr
+}
+  
+return sum
+}
+
+alert(sumInput())
 /*
 
 🔹 STEP 1: Computer choice
